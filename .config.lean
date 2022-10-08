3c9301a8b7c1d2fea5659bde3cb3bb33f5dc7c71
jsztdu <15980234+jsztdu@users.noreply.github.com>
2022-10-09 06:59:07 +0800

    Update .config.lean

--- .config.lean
+++ .config.lean
@@ -24,9 +24,10 @@ CONFIG_PACKAGE_luci-app-xlnetacc=m
 
 CONFIG_PACKAGE_ipv6helper=y
 
-CONFIG_PACKAGE_dropbear=y
+CONFIG_PACKAGE_dropbear=n
 CONFIG_PACKAGE_openssh-server=n
-CONFIG_PACKAGE_openssh-sftp-server=n
+CONFIG_PACKAGE_openssh-server-pam=y
+CONFIG_PACKAGE_openssh-sftp-server=y
 
 CONFIG_PACKAGE_luci-app-sqm=y
 CONFIG_PACKAGE_ddns-scripts_cloudflare.com-v4=y
